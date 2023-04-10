958ae37593b00f0999a574844a995303d8179969
silviagenome <89906885+silviagenome@users.noreply.github.com>
2023-04-10 15:06:53 +0200

    Update download_rule.sh

--- scripts/download_rule.sh
+++ scripts/download_rule.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 
+
 myosd_id=($1)
 mysamples=($2)
-subsamplefastq=$3
-subsamplereads=$4
-
 for ((i=0;i<${#mysamples[@]};i++)); do
-  GL-download-GLDS-data -g ${myosd_id[i]} -f --pattern ${mysamples[i]}
-  
-  if [ $subsamplefastq == 'true']; then
-  
-    echo "subsampling ${mysamples[i]} with seqtk"
-    
-    mv ${mysamples[i]} temp.fq.gz
-    
-    gunzip temp.fq.gz
-    
-    seqtk sample -s 123 temp.fq $subsamplereads > sub_temp.fq
-    
-    rm temp.fq
-    
-    gzip sub_temp.fq
-    
-    mv sub_temp.fq.gz ${mysamples[i]}
-    
-    rm sub_temp.fq.gz
-  
-  fi
-  
+echo ${mysamples[i]}
+GL-download-GLDS-data -g ${myosd_id[i]} -f --pattern ${mysamples[i]}
+wait
+mv ./${mysamples[i]} -t rawReads/
 done
