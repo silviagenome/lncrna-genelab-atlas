d11622c4d5d9134d3503e0f460362ad6217d12da
silviagenome <89906885+silviagenome@users.noreply.github.com>
2023-04-27 10:00:14 +0200

    Update download_rule.sh

--- scripts/download_rule.sh
+++ scripts/download_rule.sh
@@ -5,7 +5,8 @@ myosd_id=($1)
 mysamples=($2)
 for ((i=0;i<${#mysamples[@]};i++)); do
 echo ${mysamples[i]}
-GL-download-GLDS-data -g ${myosd_id[i]} -f --pattern ${mysamples[i]}
+sample=${mysamples[i]#rawReads/}
+GL-download-GLDS-data -g ${myosd_id[i]} -f --pattern ${sample}
 wait
 mv ./${mysamples[i]} -t rawReads/
 done
