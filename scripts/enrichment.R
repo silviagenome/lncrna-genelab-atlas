919151c540cf77e70e5a48b5b27921e84523b92a
silviagenome <89906885+silviagenome@users.noreply.github.com>
2023-07-12 21:34:27 +0200

    Update enrichment.R

--- scripts/enrichment.R
+++ scripts/enrichment.R
@@ -2,24 +2,25 @@ library(biomaRt)
 library(dplyr)
 library(clusterProfiler)
 library(org.Mm.eg.db)
+library(ggplot2)
+library(tidyverse)
 
-
-# Crear una función para obtener los genes en el rango especificado
+#Creating a function to retrieve genes within the specified range
 get_genes_in_range <- function(ens_ids, distance) {
-  # Cargar el objeto Mart
+  # Loading the Mart object.
   ensembl <- useMart("ENSEMBL_MART_ENSEMBL", dataset = "mmusculus_gene_ensembl", host = "asia.ensembl.org")
   
-  # Obtener información de los genes de interés en una sola consulta
+  # Retrieving information for the genes of interest in a single query.
   info <- getBM(attributes = c("ensembl_gene_id", "external_gene_name", "chromosome_name", "start_position", "end_position", "strand"),
                 filters = "ensembl_gene_id",
                 values = ens_ids,
                 mart = ensembl)
   
-  # Calcular posiciones de inicio y finalización considerando la distancia
+  # Calculating start and end positions, taking distance into consideration.
   info$start_position <- info$start_position - distance
   info$end_position <- info$end_position + distance
   
-  # Obtener los genes en el rango especificado
+  # Retrieving the genes within the specified range.
   genes_in_range <- character()
   for (i in 1:nrow(info)) {
     genes <- getBM(attributes = "ensembl_gene_id",
@@ -28,51 +29,80 @@ get_genes_in_range <- function(ens_ids, distance) {
                    mart = ensembl)$ensembl_gene_id
     genes_in_range <- c(genes_in_range, genes)
   }
-  
+
   return(unique(genes_in_range))
 }
 
-genes_in_range <- c()
+# loading tsv tables
+table <- read.table("/home/alumno15/TFM/results-keep-lnc/lncrna-Ensembl-Genes-109-spaceflight-vs-control.diffexp-[analysis number]-[study number].tsv", sep="\t", header=TRUE)
+table_verification <- read.table("/home/alumno15/TFM/results-tablas/spaceflight-vs-control.diffexp-[analysis number]-[study number].tsv", sep="\t", header=TRUE)
 
-# Cargar la tabla TSV
-tabla <- read.table("/home/alumno15/TFM/results-keep-lnc/lncrna-Ensembl-Genes-109-spaceflight-vs-control.diffexp-1-168.tsv", sep="\t", header=TRUE)
-tabla_verificacion <- read.table("/home/alumno15/TFM/results-tablas/spaceflight-vs-control.diffexp-1-168.tsv", sep="\t", header=TRUE)
+# Filtering of genes based on the specified criteria.
+ens_ids_up <- tabla$ensembl_gene_id[tabla$padj < 0.05 & tabla$log2FoldChange > 1]
+ens_ids_down <- tabla$ensembl_gene_id[tabla$padj < 0.05 & tabla$log2FoldChange < - 1]
 
-
-# Filtrar genes según los criterios especificados
-ens_ids <- tabla$ensembl_gene_id[tabla$padj < 0.05 & (tabla$log2FoldChange > 1 | tabla$log2FoldChange < -1)]
-
-
-# Obtener los genes en el rango especificado
+# Obtaining the genes within the specified range.
 distance <- 100000
-genes_in_range <- get_genes_in_range(ens_ids, distance)
+genes_in_range <- c()
+genes_in_range <- get_genes_in_range(ens_ids_[up/down], distance)
 
-# Filtrar los genes de genes_in_range que aparecen en tabla_verificacion
-genes_finales <- genes_in_range[genes_in_range %in% tabla_verificacion$gene]
-
-# Filtrar la tabla_verificacion según las condiciones requeridas
-tabla_verificacion_filtrada <- tabla_verificacion[tabla_verificacion$padj < 0.05 &
-                                       (tabla_verificacion$log2FoldChange > 1 | tabla_verificacion$log2FoldChange < -1), ]
-
-# Obtener la lista de genes en genes_in_range que se encuentran en la tabla filtrada
-genes_finales <- intersect(genes_in_range, tabla_verificacion_filtrada$gene)
+# Obtaining the list of genes in genes_in_range that are present in the verification table.
+final_genes <- intersect(genes_in_range, tabla_verificacion$gene)
 
 ##GO ENRICHMENT##
-enrich_result <- enrichGO(gene          = genes_finales,
-                          OrgDb         = org.Mm.eg.db,  # Base de datos específica para Mus musculus
-                          keyType       = "ENSEMBL",
-                          ont           = "BP",          # Ontología biológica de procesos (BP)
-                          pvalueCutoff = 0.05,          # Umbral de significancia para el valor de p
-                          qvalueCutoff = 0.05,
-                          pAdjustMethod = "BH")          # Umbral de significancia para el q-value ajustado
 
-# Imprimir los resultados de enriquecimiento de GO
+enrich_result <- enrichGO(gene          = final_genes,
+                          OrgDb         = org.Mm.eg.db,  
+                          keyType       = "ENSEMBL",
+                          ont           = "ALL",         
+                          pvalueCutoff = 0.05,          
+                          qvalueCutoff = 0.05,
+                          pAdjustMethod = "BH")          
+
+# Printing the results of the GO enrichment.
 print(enrich_result)
 
-####GRÁFICOS####
+##GO ENRICHMENT PLOTTING##
 
-# Realizar análisis de enriquecimiento funcional
+enrich_result <- as.data.frame(enrich_result)
 
-library(enrichplot)
-barplot(enrich_result, showCategory=20) + labs(x = "Genes Count", y = "GO term")
-dotplot(enrich_result, showCategory=30) + labs(y = "GO term")
+enrich_result$`-log10pvalue` <- -10 * log10(enrich_result$pvalue)
+
+enrich_result %>% arrange(across(.cols=c(ONTOLOGY, `-log10pvalue`))) %>%
+  group_by(ONTOLOGY) %>%       # Agrupar por ontología
+  slice_max(n = 5, order_by = `-log10pvalue`) %>%        # Mantener solo los 5 primeros términos por ontología
+  ungroup() %>%               # Desagrupar
+  rowid_to_column() %>% 
+  ggplot()  +
+  geom_bar(aes(y =  reorder(Description, rowid),  x = `-log10pvalue`, fill = ONTOLOGY), stat = "identity",  width = 0.7) +
+  scale_fill_manual(values = c("BP" = "#009E73", "CC" = "#D55E00", "MF" = "#CC79A7")) +
+  theme_bw() +
+  xlab("Enrichment score(-log10p-value)") +
+  ylab("GO enrichment terms") +
+  theme(legend.title = element_blank(),
+        axis.text.x = element_text(size = 16),
+        axis.text.y = element_text(size = 16))
+
+##WikiPathways ENRICHMENT##
+
+#Performing the conversion of Ensembl IDs to gene symbols using the org.Mm.eg.db database.
+gene_symbols <- bitr(final_genes, fromType = "ENSEMBL", toType = "ENTREZID", OrgDb = org.Mm.eg.db)
+
+enrich_wp <- clusterProfiler::enrichWP(
+  gene_symbols$ENTREZID,
+  organism = "Mus musculus",
+  pAdjustMethod = "BH",
+  pvalueCutoff = 0.05, #p.adjust cutoff; relaxed for demo purposes
+)
+
+##WikiPathways PLOTTING##
+
+enrich_wp <- as.data.frame(enrich_wp)
+
+enrich_wp$`-log10pvalue` <- -10 * log10(enrich_wp$pvalue)
+
+ggplot(ewp.up, aes(x = `-log10pvalue`, y = Description)) +
+  geom_bar(stat = "identity", fill = "#56B4E9") +
+  labs(x = "Enrichment score(-log10p-value)", y = "wikiPathways enrichment terms") +
+  theme(axis.text.x = element_text(size = 16),
+        axis.text.y = element_text(size = 16))
